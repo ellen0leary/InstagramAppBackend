4b97c5440132b2f7833b8f881466ae67ce9c6da9
Ellen <eol50559@gmail.com>
2021-03-27 11:08:20 +0000

    start of search

--- api/user/index.js
+++ api/user/index.js
@@ -1,46 +1,62 @@
-import e from 'express';
 import express from 'express';
 import User from './userModel.js';
 // import jwt from 'jso'
 
 const router = express.Router();
 
-router.get('/', (req, res,next) => {
-    User.find().then(users =>  res.status(200).json(users)).catch(next);
+router.get('/', (req, res, next) => {
+    User.find().then(users => res.status(200).json(users)).catch(next);
 });
 
-router.post('/', (req, res, next)=>{
+router.post('/', async (req, res, next) => {
     console.log(req.body.userName);
     console.log(req.body.password);
-    if(!req.body.userName || !req.body.password){
+    const user = await User.findByUserName(req.body.userName)
+    if (!req.body.userName || !req.body.password) {
         res.status(401).json({
             success: false,
             msg: 'Please pass username and password.',
-          });
-    }else{
-        const user = User.findByUserName(req.body.userName)
-        console.log(user)
-        if(!user ){
+        });
+    } else {
+        if (!user) {
             res.status(401).json({
                 success: false,
                 msg: 'No account .',
-              });
-        }else if( user.findPassword != req.body.password){
-            console.log(user.findPassword)
-            res.status(401).json({
-                success: false,
-                msg: ' incorect password.',
-              });
-        } else{
-        res.status(200).json({
-            success: true,
-            msg: 'You have logged in.',
-          });
-        }
-    }
-    
+            });
+        } else {
+            user.comparePassword(req.body.password, (err, isMatch) => {
+                if (isMatch && !err) {
+                    // return the information including token as JSON
+                    res.status(200).json({
+                        success: true,
+                        // token: 'BEARER ' + token,
+                        msg : "You have logged in"
+                    });
+                } else {
+                    res.status(401).json({
+                        success: false,
+                        msg: ' incorect password.',
+                    });
+                }
+            });
+        }}
 });
 
+
+router.get('/search', (req,res,next) => {
+    console.log(req.query.user)
+    const user = User.findByUserName(req.query.user);
+    console.log(user.userName)
+    if(!user){
+        res.status(400).json( {
+            success : false,
+            msg: "No"
+        })
+    } else{
+        res.status(200).json( user)
+    }
+    
+})
 // router.put();
 // router.delete();
 
--- api/user/userModel.js
+++ api/user/userModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
-
+import bcrypt from 'bcrypt-nodejs';
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -12,13 +12,46 @@ const UserSchema = new Schema({
 });
 
 UserSchema.statics.findByUserName = function (username) {
-    return this.findOne({ username: username });
+    return this.findOne({ userName: username });
+  }; 
+  UserSchema.statics.findByid = function (id) {
+    return this.findOne({ _id: id });
   }; 
 
-  UserSchema.method.findPassword = function () {
-      console.log(this)
-    return this.password;
-  }; 
+  UserSchema.methods.comparePassword = function(passw, cb) {
+    bcrypt.compare(passw, this.password, (err, isMatch) => {
+        console.log(passw)
+        console.log(this.password)
+        if (err) {
+            return cb(err);
+        }
+        cb(null, isMatch);
+    });
+};
+
+
+  UserSchema.pre('save', function(next) {
+    const user = this; 
+    console.log("starting" +user.userName)
+        bcrypt.genSalt(10, (err, salt)=> {
+            if (err) {
+                console.log("gensalt " + err)
+                return next(err);
+            }
+            bcrypt.hash(user.password, salt, null, (err, hash)=> {
+                if (err) {
+                    console.log("hash error " +err)
+                    return next(err);
+                }
+                console.log("hash " + hash)
+                user.password = hash;
+                console.log(user.password)
+                next();
+            });
+
+        });
+        console.log(user.password)
+    } );
 
 export default mongoose.model('User', UserSchema);
 
